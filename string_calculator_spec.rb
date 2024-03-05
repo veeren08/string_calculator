4c54038d62d676b486232a5a4cc1bb2b1ba492f2
veeren08 <veerendragupta52525@gmail.com>
2024-03-05 16:20:09 +0530

    Write tests for two numbers input

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it "returns the number for a single number" do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
   end
 end
