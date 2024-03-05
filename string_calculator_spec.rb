cbf28631132e7dc515b57f8c0831b2554f63fa20
veeren08 <veerendragupta52525@gmail.com>
2024-03-05 16:21:29 +0530

    Write tests for unknown amount of numbers input

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe StringCalculator do
     it "returns the sum of two numbers" do
       expect(StringCalculator.add("1,5")).to eq(6)
     end
+
+    it "returns the sum of unknown amount of numbers" do
+      expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+    end
   end
 end
